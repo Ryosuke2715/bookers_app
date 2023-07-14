c14fe433d92f1c2a5d278270c899bbde4fbea2c2
RyosukeSuzuki <138420782+Ryosuke2715@users.noreply.github.com>
2023-07-14 13:09:37 +0000

    [Update] 課題２更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,29 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
   end
 
   def new
@@ -10,14 +33,8 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
-  end
-
-  def show
-    @books = Book.find(params[:id])
-  end
-
-  def edit
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
   end
 
   private
